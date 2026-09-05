4d6162a9f3a84c8db8dfc3c0abb807ba19ad984a
Damisetty Tapan <damisettytapan@gmail.com>
2026-09-05 11:59:39 +0100

    Exercise 6

--- d-1/assignment_6.sv
+++ d-1/assignment_6.sv
@@ -1,3 +1,10 @@
+"Assume you have four variables ( a, b,c, and d )  in 
+your testbench top. a and b are of the 8-bit reg type, 
+while c and d are of the integer type. initialize a,b,c, 
+and d to values of 12, 34, 67, and 255 respectively. 
+Add a code to print the values of all the variables after 12 nSec"  
+
+
 `timescale 1ns/1ps
 
 module top();
