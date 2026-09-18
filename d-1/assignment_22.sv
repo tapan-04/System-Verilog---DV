c5c436d845171839a62db1edd4d7bba66ac8dc70
Damisetty Tapan <damisettytapan@gmail.com>
2026-09-18 13:00:19 +0100

    Using if-else with Constraint

---
+++ d-1/assignment_22.sv
@@ -0,0 +1,50 @@
+"Generate values between 0 to 7 for addr signal when wr is high and values 
+between 8 to 15 when wr is low. Generator code is mentioned in the Instruction 
+tab. Verify your code for 20 iterations by sending values of both wr and addr 
+on a console.
+
+class generator;
+  
+  rand bit [3:0] addr;
+  rand bit wr;
+  
+  /////////////////add constraint 
+  
+  
+  
+  
+  
+endclass
+ 
+/////////////////Add testbench top code"
+
+--------------
+
+class generator;
+
+  rand bit [3:0] addr;
+  rand bit wr;
+
+  constraint addr_c {
+    if (wr == 1)
+      addr inside {[0:7]};
+    else
+      addr inside {[8:15]};
+  }
+
+endclass
+
+module tb;
+
+  generator g;
+
+  initial begin
+
+    g = new();
+    
+    for (int i = 0; i < 20; i++) begin
+      assert(g.randomize());
+      $display("Iteration : %0d | wr : %0d | addr : %0d", i, g.wr, g.addr);
+    end
+  end
+endmodule
