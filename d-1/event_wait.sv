659329a9c90b0645b5005545af39b5b592b3c382
Damisetty Tapan <damisettytapan@gmail.com>
2026-09-18 13:39:28 +0100

    Using Wait Event

---
+++ d-1/event_wait.sv
@@ -0,0 +1,16 @@
+module tb;
+  
+  event a1,a2;
+  
+  initial begin 
+    -> a1;
+    -> a2;
+  end 
+  
+  initial begin
+    wait(a1.triggered);
+    $display("Event a1 triggered");
+    wait(a2.triggered);
+    $display("Event a2 triggered");
+  end
+endmodule
